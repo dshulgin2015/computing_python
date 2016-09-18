84380a8664cd657d317e59a2998288bcedd77958
dshulgin2015 <unetby2016@yandex.ru>
2016-09-18 19:41:35 +0300

    sosach parser

--- 2chAPItest.py
+++ 2chAPItest.py
@@ -1,16 +1,28 @@
+# -*- coding: utf-8 -*-
 import urllib2
 import json
-
+import datetime
+import re
 
 bel_board = []
 response = json.loads(urllib2.urlopen(
-    'https://2ch.hk/by/threads.json').read())
+    'https://2ch.hk/psy/threads.json').read())
 # print response["threads"][1]
 for i in xrange(0, len(response["threads"])):
     # print response["threads"][i]["subject"].encode('utf-8'),
     # response["threads"][i]["views"]
     bel_board.append({"views": response["threads"][i][
         "views"], "posts_count": response["threads"][i]["posts_count"],
-        "title": response["threads"][i]["subject"].encode('utf-8')})
+        "subject": response["threads"][i]["subject"].encode('utf-8'),
+        "timestamp": response["threads"][i]["timestamp"]})
 
-print bel_board[1]['views']
+
+for i in xrange(0, len(bel_board)):
+    date = datetime.datetime.fromtimestamp(
+        int(bel_board[i]['timestamp']))
+    cur_date = datetime.datetime.fromtimestamp(
+        int('1474070400'))
+    if date <= cur_date:
+        subject = bel_board[i]["subject"]
+        if re.search('^.*(окр)', subject) is not None:
+            print subject
